53ed3aada4b83bb8617c40e50c97b8639e313e3c
Ivan Premuzic <ivan.premuzic@gmail.com>
2025-03-03 01:30:11 +0100

    Add test structure
    Adds general test structure without test implementation.
    TODO:
    Write actual tests, mock GPIO input state and check output state.

---
+++ test/app_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "middleware.hpp"
+#include "hal_mock.hpp"
+
+/* TODO: Write actual tests */
+TEST(AppTest, test_test)
+{
+	EXPECT_EQ(1+1, 2);
+}
